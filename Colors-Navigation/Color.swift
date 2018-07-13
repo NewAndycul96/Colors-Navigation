7fa344b02d04f2e523d3f48060f4c77a9b2b9858
Anand Kulkarni <anandkulkarni@Anands-MacBook-Pro.local>
2018-07-13 11:01:13 -0500

    Finished Challenge

---
+++ Colors-Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors-Navigation
+//
+//  Created by Anand Kulkarni on 7/13/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
--- Colors-Navigation/ColorsNavViewController.swift
+++ Colors-Navigation/ColorsNavViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ColorsNavViewController: UIViewController {
+class ColorsNavViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +28,22 @@ class ColorsNavViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        let color = colors[indexPath.row]
+        
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
